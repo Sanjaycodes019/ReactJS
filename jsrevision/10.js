25f9efccb1410c91ebc73939f243ea7c66661c90
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-02-13 05:08:31 +0000

    commit

---
+++ jsrevision/10.js
@@ -0,0 +1,13 @@
+// destructuring
+
+const course = {
+    courseName : "JS in Nepali",
+    price : 999,
+    courseTeacher : "Sanjay Gupta"
+}
+
+//console.log(course.courseTeacher);
+
+const {courseTeacher : instructor} = course;
+// console.log(instructor);
+
