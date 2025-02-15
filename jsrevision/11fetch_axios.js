43f359f661bab5c167d5918fe6de6720a53c7a96
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-02-15 15:50:35 +0000

    commit

--- jsrevision/11fetch_axios.js
+++ jsrevision/11fetch_axios.js
@@ -12,10 +12,12 @@ const url = "https://cat-fact.herokuapp.com/facts";
 const getFacts = async() => {
     console.log("getting data...");
     let response = await fetch(url);
-    console.log(response); // JSON formate
+    // console.log(response); // JSON formate
+    let data = await response.json();
+    console.log(data[0].text);
 }
 
-// getFacts();
+ getFacts();.
 
 
 // understanding terms
