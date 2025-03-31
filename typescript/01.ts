b5fd56b73aa47d2ba05cb1438b8e3c5846b9bf35
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-03-31 11:07:38 +0000

    commit

---
+++ typescript/01.ts
@@ -0,0 +1,11 @@
+// Basic types:
+// Primitive types -> (number, boolean, string)
+// Arrays
+// Tuples
+// Enums
+// Any, Unknown, Void, Null, Undefined, Never
+
+// primitive and reference
+// (), {}, [] -> reference (original value will be affected)
+// all other non-braces are primitive
+
