3198eefd580931ce5af6af3d045dfb221c4f9053
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-03-31 11:40:33 +0000

    commit

--- typescript/01.ts
+++ typescript/01.ts
@@ -9,3 +9,63 @@
 // (), {}, [] -> reference (original value will be affected)
 // all other non-braces are primitive
 
+// variables
+// var, let, const
+// var not used
+
+// Arrays
+
+// let arr = [1, 2, 3, {name: sanjay}];
+// let brr: number[] = [1, 2, 4]
+
+// tuples
+// let arr: [string, number] = ["sanjay", 21]; 
+
+// Enums -> enumerations
+// enum userRoles{
+//    ADMIN = "admin",
+//    GUEST = "guest",
+//    SUPER_ADMIN = "super_admin"
+// }
+
+// enum StatusCodes{
+//     ABANDONED = "abandoned status code 500",
+//     NOTFOUNT = "not found status code 404"
+// }
+
+// let a : number;
+
+// ensure that no any variable has type any;
+// any happens when the variable is unknownly declared
+// with any, typescript is off
+
+// example
+// let a;
+// a = "Sanjay";
+// a = 21;
+
+// if the type is unknown, no any function works over there
+
+// function abc(): void{
+//     console.log("hello, Sanjay");
+// }
+
+// if the function is not returning anyth
+
+// function abc(): number{
+// return 12;
+// }
+
+// null type
+// let a : null;
+// a = 12; 'this gives error'
+
+// Union
+// let a : string | null;
+
+// never
+// returns nothing
+// function abc(){
+//     while(true) {}
+// }
+
